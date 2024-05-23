cc907082abd8fae66f39c1a765bbf39a06ac2c56
astovali <connor.mcferran@student.education.wa.edu.au>
2024-05-23 14:26:50 +0800

    Fixed repeated letters Fixed invalid words only being considered "not in wordlist"

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from random import choices
+from random import sample
 from colorama import init as colorama_init
 from colorama import Fore
 from colorama import Style
@@ -22,34 +22,42 @@ def valid_word(word):
 
 max_score = 0
 while max_score == 0:
-    letters = choices('abcdefghijklmnopqrstuvwxyz', k=7)
+    letters = sample('abcdefghijklmnopqrstuvwxyz', k=7)
     wordlist = set(filter(valid_word, all_wordlist))
     max_score = sum(map(len, wordlist))
 
 score = 0
-guessed_words = []
+guessed_words = set()
 print(f"Letters: {Fore.RED+letters[0]+Style.RESET_ALL}, {', '.join(letters[1:])}")
 print(f"Max possible score: {max_score}")
 
 while score < max_score:
     guess = input('')
-    if guess not in wordlist:
+    if guess == '':
+        break
+    if len(guess) < 4:
+        print(f"{guess} is too short")
+        continue
+    if guess not in all_wordlist:
         print(f"{guess} is not in the wordlist")
         continue
     if guess in guessed_words:
         print(f"{guess} has been guessed")
         continue
-    if len(guess) < 4:
-        print(f"{guess} is too short")
     valid = True
     has_main_letter = False
     for letter in guess:
-        if letter not in letter:
+        if letter not in letters:
             print(f"'{letter}' is not allowed")
             valid = False
         if letter == letters[0]:
             has_main_letter = True
+    if not has_main_letter:
+        print(f"{guess} doesn't include '{letters[0]}'")
     if valid and has_main_letter:
         score += len(guess)
-        guessed_words.append(guess)
+        guessed_words.add(guess)
         print(f"Score: {score}")
+
+print("You missed: ")
+print(', '.join(wordlist - guessed_words))
